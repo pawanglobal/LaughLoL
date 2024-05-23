6cddc540882d3f01f129337f106e5ca5bee3dee0
pawanglobal07 <pawanglobal07@gmail.com>
2024-05-23 03:35:16 +0200

    first commit

---
+++ DA/DataAnalysisBootCamp/03_GROUP_BY_ORDER_BY.sql
@@ -0,0 +1,20 @@
+-- GROUP BY 
+-- AGGREGATED FUNCTIONS AVG, MAX, MIN, COUNT
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender;
+
+SELECT salary, occupation, MAX(salary)
+FROM employee_salary
+GROUP BY salary, occupation;
+
+-- ORDER BY
+SELECT *
+FROM employee_demographics
+ORDER BY first_name DESC;
+
+SELECT *
+FROM employee_demographics
+ORDER BY gender, age DESC;
+
