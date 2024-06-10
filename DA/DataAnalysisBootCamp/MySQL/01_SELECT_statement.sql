f9e602fcfb68bc21ed7536dedfe5e82be075b317
pawanglobal07 <pawanglobal07@gmail.com>
2024-06-10 21:16:19 +0200

    More practice

--- DA/DataAnalysisBootCamp/MySQL/01_SELECT_statement.sql
+++ DA/DataAnalysisBootCamp/MySQL/01_SELECT_statement.sql
@@ -1,16 +1,34 @@
+-- To see all the data
+-- First way to see it
+
 SELECT *
 FROM employee_demographics;
 
 SELECT *
+FROM employee_salary;
+
+-- SELECT statement with more specifity
+-- To select all the data
+
+SELECT *
 FROM parks_and_recreation.employee_demographics;
+
+-- To select one column
 
 SELECT first_name
 FROM parks_and_recreation.employee_demographics;
+
+SELECT last_name
+FROM employee_demographics;
+
+-- To select Multiple columns
 
 SELECT first_name,
 age,
 gender
 FROM parks_and_recreation.employee_demographics;
+
+-- Playing with the data of colums with SELECT statement
 
 SELECT first_name,
 age,
@@ -18,7 +36,14 @@ age + 10,
 gender
 FROM parks_and_recreation.employee_demographics;
 
-#PEMDAS
+-- PEMDAS
+-- How mathematical operations work in SQL
 
 SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics;
+
+-- Analyze this query
+-- Age is not getting affected by gneder
+
+SELECT DISTINCT gender, age
 FROM parks_and_recreation.employee_demographics;
