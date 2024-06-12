03c7c660c571a0645110a5a9a6aeb748de2ab0fc
pawanglobal07 <pawanglobal07@gmail.com>
2024-06-12 22:47:28 +0200

    More practice

--- DA/DataAnalysisBootCamp/MySQL/02_WHERE_clause.sql
+++ DA/DataAnalysisBootCamp/MySQL/02_WHERE_clause.sql
@@ -38,3 +38,21 @@ WHERE first_name Like 'A%';
 SELECT *
 FROM parks_and_recreation.employee_demographics
 WHERE first_name Like 'A____';
+
+-- Let's do more practice
+
+SELECT *
+FROM parks_and_recreation. employee_salary;
+
+-- Little complex query
+
+SELECT employee_id, first_name, salary
+FROM parks_and_recreation. employee_salary
+WHERE (employee_id)%2 =0 AND salary> 25000 OR first_name LIKE 'A%';
+
+SELECT first_name, salary
+FROM parks_and_recreation.employee_salary
+WHERE first_name NOT LIKE 'A%' AND first_name != 'RON'  -- Don't use OR for this -> satisfies one condition -> unable to filter
+AND salary BETWEEN 50000 AND 70000; -- Both inclusive
+
+
