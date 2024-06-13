b58dea7d1fe0e81c6fa0e66f0e85b2e586722548
pawanglobal07 <pawanglobal07@gmail.com>
2024-06-13 23:05:52 +0200

    More practice

--- DA/DataAnalysisBootCamp/MySQL/04_HAVING_VS_WHERE.sql
+++ DA/DataAnalysisBootCamp/MySQL/04_HAVING_VS_WHERE.sql
@@ -15,3 +15,24 @@ FROM employee_salary
 WHERE occupation LIKE '%manager%'
 GROUP BY occupation
 Having AVG(salary) > 50000;
+
+-- More Practice
+SELECT dept_id, AVG(salary)
+FROM parks_and_recreation.employee_salary
+GROUP BY dept_id
+HAVING AVG(salary) BETWEEN 25000 AND 60000;
+
+SELECT *
+FROM parks_and_recreation.employee_salary;
+
+
+-- The HAVING clause should come after the GROUP BY clause but before the ORDER BY clause.
+
+SELECT dept_id, AVG(salary) AS avg_salary
+FROM parks_and_recreation.employee_salary
+WHERE dept_id IS NOT NULL
+GROUP BY dept_id
+HAVING AVG(salary) > 20000
+ORDER BY avg_salary DESC;
+
+
