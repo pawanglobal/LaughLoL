63bb480a6d9553c726f5921d2d9b10e897aba3fd
pawanglobal07 <pawanglobal07@gmail.com>
2024-06-13 22:25:31 +0200

    More practice

--- DA/DataAnalysisBootCamp/MySQL/03_GROUP_BY_ORDER_BY.sql
+++ DA/DataAnalysisBootCamp/MySQL/03_GROUP_BY_ORDER_BY.sql
@@ -18,3 +18,21 @@ ORDER BY first_name DESC;
 SELECT *
 FROM employee_demographics
 ORDER BY gender, age DESC;
+
+-- More practice
+
+SELECT * 
+FROM parks_and_recreation.employee_salary;
+
+-- Select column with the SELECT statement before using GROUP BY
+
+SELECT dept_id, AVG(salary), COUNT(salary)
+FROM parks_and_recreation.employee_salary
+GROUP BY dept_id;
+
+-- ORDER BY
+SELECT *
+FROM parks_and_recreation.parks_departments
+WHERE (department_id) % 2 = 0 
+ORDER BY department_id DESC;
+
