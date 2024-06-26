df2957d195e271dcc4991e77c88f91d4b32960e9
pawanglobal07 <pawanglobal07@gmail.com>
2024-06-26 21:24:16 +0200

    Intial commit

---
+++ DA/DataAnalysisBootCamp/SQL_problem_solving/Introductory_problems/08_prob.sql
@@ -0,0 +1,6 @@
+-- Looking at the Orders table, there’s a field called ShipCountry. Write a query that shows the OrderID, CustomerID, and ShipCountry for the
+-- orders where the ShipCountry is either France or Belgium
+
+SELECT OrderID, CustomerID, ShipCountry
+FROM northwind.orders
+WHERE ShipCountry = 'France' OR ShipCountry = 'Belgium';
